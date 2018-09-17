f609e6241b573b0a710ec271837b5a9d44a221ed
daniel <daniel10427@gmail.com>
2018-09-18 01:46:30 +0800

    Add EditText to Main

--- app/src/main/java/com/example/mana1/sampletest/location.java
+++ app/src/main/java/com/example/mana1/sampletest/location.java
@@ -5,27 +5,29 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
 public class location extends AppCompatActivity {
 
     public static String newresult01;
     public static String newresult02;
     private Button mbutton;
+    private EditText metxtLocation, metxtDestination;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
-        final TextView text_1=(TextView)findViewById(R.id.text1);
-        final TextView text_2=(TextView)findViewById(R.id.text2);
+
+        metxtLocation=(EditText)findViewById(R.id.etxtLocation);
+        metxtDestination=(EditText)findViewById(R.id.etxtDestination);
 
         mbutton=(Button)findViewById(R.id.button);
         mbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                newresult01=text_1.getText().toString();
-                newresult02=text_2.getText().toString();
+                newresult01=metxtLocation.getText().toString();
+                newresult02=metxtDestination.getText().toString();
                 startActivity(new Intent(location.this, MapsActivity.class));
             }
         });
